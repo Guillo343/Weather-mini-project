7cb2cc3ee4892412bef92f4f3fcdeede79cb3e3b
Guillermo Muñoz <guillemunozi2003@gmail.com>
2024-02-07 12:54:24 -0500

    Summary

--- weather/src/Components/WeatherApp/WeatherApp.jsx
+++ weather/src/Components/WeatherApp/WeatherApp.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import './Weather.css'
-import cloudy from '../../assets/cloudy.png'
-import cold from '../../assets/cold.png'
-import drizzle from '../../assets/drizzle.png'
-import humidity from '../../assets/humidity.png'
-import rain from '../../assets/rain.png'
-import search from '../../assets/search.png'
-import snowy from '../../assets/snowy.png'
-import sun from '../../assets/sun.png'
+ import cloudy from '../../assets/cloudy-day.png'
+ import wind from '../../assets/wind.png'
+ import drizzle from '../../assets/drizzle.png'
+ import humidity from '../../assets/humidity-logo.png'
+  import rain from '../../assets/rainy.png'
+ import search from '../../assets/search.png'
+ import snow from '../../assets/snowflake.png'
+ import sun from '../../assets/sun.png'
 
 
 export const WeatherApp = () => {
   return (
-    <div>WeatherApp
-      <img src={rain} alt="Cloudy" />
+    <div className='container-weather'>
+      <div className="top-bar">
+        <input type='text' className='cityInp' placeholder='Write a City'/>
+        <div className="search-icon">
+          <img src={search} alt="search-logo" />
+        </div>
+      </div>
     </div>
   )
 }
