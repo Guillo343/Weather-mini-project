fe6341a819b161621175aefedbe35ec24e9f2d1b
Guillermo Muñoz <guillemunozi2003@gmail.com>
2024-02-07 12:08:27 -0500

    Summary

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import { WeatherApp } from './Components/WeatherApp/WeatherApp'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='container-app'>
+      <WeatherApp />
+    </div>
   )
 }
 
---
+++ weather/src/Components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Weather.css'
+import cloudy from '../../assets/cloudy.png'
+import cold from '../../assets/cold.png'
+import drizzle from '../../assets/drizzle.png'
+import humidity from '../../assets/humidity.png'
+import rain from '../../assets/rain.png'
+import search from '../../assets/search.png'
+import snowy from '../../assets/snowy.png'
+import sun from '../../assets/sun.png'
+
+
+export const WeatherApp = () => {
+  return (
+    <div>WeatherApp
+      <img src={rain} alt="Cloudy" />
+    </div>
+  )
+}
